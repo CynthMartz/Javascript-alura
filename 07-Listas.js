9d2aef8de2968f5df624a461570f39adf0caba3a
CynthMartz <carol.martz21@gmail.com>
2023-04-24 12:33:03 -0400

    Agregar, eliminar y sustituir elementos

---
+++ 07-Listas.js
@@ -0,0 +1,30 @@
+const ciudad1 = "Bogotá";
+const ciudad2 = "Lima";
+const ciudad3 = "Santiago";
+
+//Definición de una lista con tipo de dato alfanumérico
+const ciudadesDisponibles = new Array("Bogotá","Lima","Santiago");
+//console.log(ciudadesDisponibles);
+
+//Definición de una lista de forma abreviada
+const paisesDisponibles = ["Colombia","Perú","Chile","Brasil","Argentina"];
+//console.log(paisesDisponibles);
+
+//Agrega elementos al final de la lista
+paisesDisponibles.push('Uruguay');
+ciudadesDisponibles.push('Montevideo');
+
+//Agrega elementos al inicio de la lista
+paisesDisponibles.unshift('Ecuador');
+ciudadesDisponibles.unshift('Quito');
+
+console.log(ciudadesDisponibles);
+console.log(paisesDisponibles);
+
+//Mostrando el primer elemento de la lista
+console.log(paisesDisponibles[0]);
+console.log(ciudadesDisponibles[0]);
+
+//Elimina y sustituye
+paisesDisponibles.splice(1,2,'Venezuela','Paraguay');
+console.log(paisesDisponibles);
